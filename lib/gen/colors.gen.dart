8274367f4c86baf075951e3b48ea0ddedd09ed8a
Dchll <15535517032@163.com>
2026-05-04 20:19:10 +0800

    chore: 更新颜色

---
+++ lib/gen/colors.gen.dart
@@ -0,0 +1,25 @@
+// dart format width=80
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: deprecated_member_use,directives_ordering,implicit_dynamic_list_literal,unnecessary_import
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class ColorName {
+  ColorName._();
+
+  /// Color: #000000
+  static const Color black = Color(0xFF000000);
+
+  /// Color: #80000000
+  static const Color black50 = Color(0x80000000);
+
+  /// Color: #000000000
+  static const Color transparent = Color(0x000000000);
+}
